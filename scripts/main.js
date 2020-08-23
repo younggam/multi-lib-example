00a6f7a795e578d0a8c1c11bfefde780232e99b2
younggam <61054554+younggam@users.noreply.github.com>
2020-08-23 17:21:37 +0900

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
 require("multi2-test");
+//prefix modname to change main.js of multi-lib to your mod's
 require("multi-lib-example/multi2-test2");
-require("multi-test");
+require("multi-lib-example/multi-test");
 require("multi-lib-example/multi-test2");
